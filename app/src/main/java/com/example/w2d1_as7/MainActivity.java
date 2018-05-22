fe5a072abcba6257481623a221fe06695ed9baef
Ross Centanni <ross.centanni@mobileappscompany.us>
2018-05-22 09:25:42 -0400

    Whoops, I uploaded the wrong thing!

--- app/src/main/java/com/example/w2d1_as7/MainActivity.java
+++ app/src/main/java/com/example/w2d1_as7/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.w2d1_as7;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-
-import com.example.w2d1_as7.R;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -31,42 +30,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Set up EditText
-    private void init(){
+    private void init() {
         nameET = findViewById(R.id.etName);
         fileName = getString(R.string.fileName);
     }
 
-    public void writeToFile(View view){
+    public void writeToFile(View view) {
         String inputName = nameET.getText().toString();
-        if(inputName != null){
-            try{
-                file = new File(this.getFilesDir(), fileName);
+        try {
+            if (checkExternal()) {
+                file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), fileName);
                 out = new FileOutputStream(file);
                 out.write(inputName.getBytes());
                 out.close();
+            } else {
+                Log.d("externalCheck", "External storage is not available.");
             }
-            catch(Exception e){
-                Log.e("Whoopsie", "Oh no!");
-            }
+        } catch (Exception e) {
+            Log.e("Whoopsie", "Oh no!");
         }
     }
 
-    public void printName(View view){
-        try{
-            in = new FileInputStream(file);
-            InputStreamReader isr = new InputStreamReader(in);
-            BufferedReader br = new BufferedReader(isr);
+    public void printName(View view) {
+        try {
+            if (checkExternal()) {
+                in = new FileInputStream(file);
+                InputStreamReader isr = new InputStreamReader(in);
+                BufferedReader br = new BufferedReader(isr);
 
-            String output = br.readLine();
-            Log.d("lastNameEntered", output);
-        }
-        catch (FileNotFoundException e){
+                String output = br.readLine();
+                Log.d("lastNameEntered", output);
+            } else {
+                Log.d("externalCheck", "External storage is not available.");
+            }
+
+        } catch (FileNotFoundException e) {
             Log.e("Whoopsie", "printName: File not found.", e.getCause());
-        }
-        catch (java.io.IOException e) {
+        } catch (java.io.IOException e) {
             Log.e("Uhoh", "printName: IO problem.", e.getCause());
         }
     }
 
-
+    private boolean checkExternal() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
 }
